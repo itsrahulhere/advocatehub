60fa490e885eafb4ef977948cff760e5b69e9cdc
instagram-com-post <theuntoldstory47@gmail.com>
2023-04-02 22:57:09 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/contact" element={ <Contact /> } />
+
+          <Route path="/*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+
+      <a href="https://wa.me/7532080633?text=Hello%2C%20i%20want%20to%20know%20about%20the%20products" className="wa" target="_blank" rel="noreferrer" >
+        <i class="fa fa-whatsapp my-wa"></i>
+      </a>
     </div>
   );
 }
